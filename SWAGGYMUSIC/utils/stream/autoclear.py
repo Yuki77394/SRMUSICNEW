524bae307b7cfc2f3fb82868eddaa29486a0fe19
Yuki77394 <pinku77394@gmail.com>
2026-09-04 07:04:46 +0530

    Update autoclear.py

--- SWAGGYMUSIC/utils/stream/autoclear.py
+++ SWAGGYMUSIC/utils/stream/autoclear.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
+# Copyright (C) 2021-2022 by Yuki77394@Github, < https://github.com/Yuki77394 >.
 #
 # This file is part of < https://github.com/TheAloneTeam/SWAGGYMUSIC > project,
 # and is released under the "GNU v3.0 License Agreement".
@@ -18,7 +18,7 @@ async def auto_clean(popped):
         autoclean.remove(rem)
         count = autoclean.count(rem)
         if count == 0:
-            if "vid_" not in rem or "live_" not in rem or "index_" not in rem:
+            if "vid_" not in rem and "live_" not in rem and "index_" not in rem:
                 try:
                     os.remove(rem)
                 except:
